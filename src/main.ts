8acef0eaaa76bb993b806c3fe9cdfb8fb2449b8d
aes-sarg <aes-sarg@e2r3p9.1337.ma>
2025-02-09 00:18:49 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { parseHtml } from './utils/vdom'
+
+
+let vdom = parseHtml( 
+    /* html */`
+    <h1 style="color:green">Hello</h1>
+    <p>This is a page</p>
+    <p>some more text</p>
+    `
+)
+
+console.log(vdom);
